c842c78b529d178bdb96f35ea77d53c094c6ec30
PillerDaniel <pdanitwitch@gmail.com>
2025-01-29 18:52:24 +0100

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hali');
+});
+
+app.get('/haligali', (req, res) => {
+    res.send('haligali');
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on 5000');
+});
