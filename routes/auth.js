1b02c1851b8bbc97499b5ddfa514fd40386a4260
PillerDaniel <pdanitwitch@gmail.com>
2025-02-07 19:39:34 +0100

    user model, auth route, registration

---
+++ routes/auth.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const config = require("config");
+const jwtSecret = config.get("jwtSecret");
+const { body, validationResult } = require("express-validator");
+
+// Register
+// /auth/register
+
+router.post(
+  "/register",
+  [
+    //validation
+    body("userName").notEmpty().withMessage("Username is required"),
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 character"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { userName, password, email } = req.body;
+
+    try {
+      // email or username already in use
+      const existingUser = await User.findOne({
+        $or: [{ email }, { userName }],
+      });
+      if (existingUser) {
+        return res
+          .status(400)
+          .json({ message: "Username or Email already in use" });
+      }
+
+      // pw hash
+      const salt = await bcrypt.genSalt(10);
+      const hashedPw = await bcrypt.hash(password, salt);
+
+      const user = new User({
+        userName,
+        password: hashedPw,
+        email,
+      });
+
+      await user.save();
+      res.status(201).json({ message: "User Created" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
+
+module.exports = router;
